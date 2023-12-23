a76e9a011754f88f8b2d4a3bf68fb5e2316f796d
INDERJEETBHADU <bishnoiinderjeet580@gmail.com>
2023-12-23 10:05:24 +0530

    first commit

---
+++ dist/app.js
@@ -0,0 +1,4 @@
+function opennav() {
+    document.getElementById("navbar").classList.toggle("!left-[0%]");
+    document.body.classList.toggle("overflow-hidden");
+}
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/**/*.{html,js}"],
+  theme: {
+
+    extend: {
+      fontFamily: {
+        'Poppins': "'Poppins' , sans-serif",
+        'Yantramanav': "'Yantramanav',sans-serif",
+      },
+
+    },
+  },
+  plugins: [],
+}
+
